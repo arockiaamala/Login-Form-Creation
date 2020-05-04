6de167c35f075b209eb4fbc9f8ba9a756d04bbbb
Arockia Amala <arockiaamala344@gmail.com>
2020-05-04 19:13:02 +0530

    Login changes

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Loginform.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public string Username { get; set; }
+        [BindProperty]
+        public string Password { get; set; }
+
+        public string msg;
+        public void OnGet()
+        {
+
+        } 
+        public IActionResult OnGetLogout()
+        {
+            HttpContext.Session.Remove("Username");
+            return Page();
+        }
+        public IActionResult OnPost()
+        {
+            if (Username.Equals("abc") && Password.Equals("abc"))
+            {
+                HttpContext.Session.SetString("Username", Username);
+                return RedirectToPage("Welcome");
+
+            }
+            else
+            {
+                msg = "Invalid";
+                return Page();
+            }
+
+        }
+    }
+}
